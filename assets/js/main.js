1fd4122042ca17ccce37baee24a57747c6539016
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-09-25 06:15:14 -0500

    complete inputs and css not ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,109 +19,82 @@ class Bus{
 		$('#lastname').val('');
 		$('#dni').val('');
 		$('#dniSearch').val('');
-		$('#result').val('');
+		$('#result').empty();
 		$('#showPlace').html('Select place');
 	}
 
 	getPassengerList() {
 		return passenger;
 	}
-	reservar(name, apellido, dni, asiento){	
+	makeReservation(name, apellido, dni, asiento){	
 		let mostrar = document.getElementById("showPlace");
 		let mos = mostrar.textContent;
 		let res = mos.split('You have selected the seat # ');
 		let seatPlace = res[1];
 	
-		function Pasajero(nombre, apellido, dni, asiento){
-			this.nombre = $("#name").val();
-			this.apellido = $("#lastname").val();
+		function Passenger(name, lastname, dni, asiento){
+			this.name = $("#name").val();
+			this.lastname = $("#lastname").val();
 			this.dni = $("#dni").val();
 			this.asiento = seatPlace;
 		}
 	
-		let nuevoP = new Pasajero(name, apellido, dni, asiento);
-		this.passenger.push(nuevoP);
-		console.log(this.passenger);
-		alert("Pasajero registrado"+ "\n"+"Nombre: " + nuevoP.nombre + "\n"+"Apellido: " + nuevoP.apellido + "\n"+ "DNI: "+ nuevoP.dni + "\n"+ "Asiento: "+ nuevoP.asiento);
-		this.restart();
-		//return true;
+		let newPassenger = new Passenger(name, apellido, dni, asiento);
+		this.passenger.push(newPassenger);
+		this.toHTML(newPassenger)
+		setTimeout(()=>{ this.restart(); }, 3000);
 	}
 
-	buscar(){
+	search(){
 		let dniSearch = $("#dniSearch").val();
 		dniSearch = parseInt(dniSearch);
 		let foundID;
-		for(var i in this.passenger){
-			if(dniSearch == this.passenger[i].dni && this.passenger[i] != undefined){
-				foundID = this.passenger[i];				
-				$("#name").val(foundID.nombre);
-				$("#lastname").val(foundID.apellido);
+		$.each(this.passenger, function (index, value) {
+			if(dniSearch == parseInt(value.dni) && value != undefined){
+				foundID = value;				
+				$("#name").val(foundID.name);
+				$("#lastname").val(foundID.lastname);
 				$("#dni").val(foundID.dni);
 				return true;
 			}
-		}
+		});
 		console.log(foundID);
-		this.restart();
+		setTimeout(()=>{ this.restart(); }, 3500);
 	}
 
-	mostrar(pasaj) {
-		this.restart();
-		var rpta = document.getElementById("result");
-		var res="";
-	
-		var nam = "<strong>Nombre: </strong>" + pasaj.nombre + "<br>";
-		var las = "<strong>Apellido: </strong>" + pasaj.apellido + "<br>";
-		var i = "<strong>DNI: </strong>" + pasaj.dni + "<br>";
-		var as = "<strong>Asiento: </strong>"+ pasaj.asiento;
-		res += "<div class='lista' align='center'>"+ nam+ las + i+ as+"</div>";
-		rpta.innerHTML += res;
+	toHTML(pasaj) {
+		$('#result').append(`<div class='lista' align='center'>\
+								<strong>Nombre: </strong>${pasaj.name}<br>\
+								<strong>Apellido: </strong>${pasaj.lastname}<br>\
+								<strong>DNI: </strong>${pasaj.dni}<br>\
+								<strong>Asiento: </strong> ${pasaj.asiento}\
+							</div>`);
 	}
 
-	listar(){
-		var res = [];
-		for(var i of this.passenger){
-			res += this.mostrar(i);
-		}
-		return res;
+	showList(){
+		$.each(this.passenger, (index, value)=> {
+			this.toHTML(value)
+		});
+		setTimeout(()=>{ this.restart(); }, 4000);
 	}
 	
-	cancelar(){
+	cancel(){
 		this.restart();
 	}
 
-	iniciar(){
-		var asientos = document.getElementsByTagName('td');
-		for (var i = 0; i < asientos.length; i++) {
-			asientos[i].addEventListener('click',this.redirect,false);
+	init(){
+		var spot = document.getElementsByTagName('td');
+		for (var i = 0; i < spot.length; i++) {
+			spot[i].addEventListener('click',this.redirect,false);
 		}
-		$('#btnReservation').click( () => this.reservar());
-		$('#btnShow').click( () => this.listar());
-		$('#btnSearch').click( () => this.buscar());
+		$('#btnReservation').click( () => this.makeReservation());
+		$('#btnShow').click( () => this.showList());
+		$('#btnSearch').click( () => this.search());
 	}
 }
 
 
 $(document).ready(()=>{
 	var app = new Bus ();
-	app.iniciar();
+	app.init();
 })
-
-/*
-var celdas = document.getElementsByTagName('td');
-for (var i = 0; i < celdas.length; i++) {
-    //celdas[i].addEventListener('click',redirect,false);
-    celdas[i].addEventListener('click',cambiarColor,false);
-} */
-/*function redirect(event){
-    document.getElementById("mostrar").innerHTML=(event.target.textContent);
-}*/
-
-/*
-function cambiarColor(){
-	var celdas = document.getElementsByTagName('td');
-	for (var i = 0; i < celdas.length; i++) {
-    //celdas[i].addEventListener('click',redirect,false);
-    	celdas[i].addEventListener('click',cambiarColor,false);
-	}
-	celda.style.backgroundColor = "#008000";
-} */
